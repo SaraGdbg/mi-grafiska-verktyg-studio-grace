d5caf8d5f0ee4b02f22bc5c733f22b1b47b3ac22
Fanny Holmström <fannyholmstrom@icloud.com>
2024-04-16 15:39:10 +0200

    :sparkles: added open menu (wip)

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import './scss/style.scss';
+
+const menuButton = document.querySelector('.menu-closed');
+const menuLinks = document.createElement('div');
+
+menuLinks.className = 'menu-links';
+menuLinks.innerHTML = `
+    <a class="hyperlink" href="#">Link</a>
+    <a class="hyperlink" href="#">Link</a>
+    <a class="hyperlink" href="#">Link</a>
+    <a class="hyperlink" href="#">Link</a>
+    <a class="hyperlink" href="#">Link</a>
+    <a class="hyperlink" href="#">Link</a>
+    <a class="hyperlink" href="#">Link</a>
+`;
+
+const openMenu = document.createElement('div');
+openMenu.className = 'open-menu hidden';
+openMenu.appendChild(menuLinks);
+document.body.appendChild(openMenu);
+
+
+menuButton.addEventListener('click', () => {
+    openMenu.classList.remove('hidden');
+});
+
