162d5ed9992012e42cc445629bedb6a8033a04f5
ReginaJM <reg.johansson@gmail.com>
2024-04-16 18:22:57 +0200

    ✨cookie, added visibility hidden on click

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './scss/style.scss';
+
+
+const cookieBtn = document.getElementById('cookieBtn');
+const cookieBar = document.getElementById('cookieBar');
+
+cookieBtn.addEventListener('click', function() {
+    console.log('Knappen klickades!');
+    cookieBar.classList.add('hidden');
+});
