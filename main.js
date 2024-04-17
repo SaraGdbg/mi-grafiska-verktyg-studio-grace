4f24fb097b34eee2e0b06df326d3b12c5137e08a
Fanny Holmström <fannyholmstrom@icloud.com>
2024-04-17 08:37:44 +0200

    :lipstick: added styling to menu links

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const menuLinks = document.createElement('div');
 
 menuLinks.className = 'menu-links';
 menuLinks.innerHTML = `
-    <a class="hyperlink" href="#">Link</a>
-    <a class="hyperlink" href="#">Link</a>
-    <a class="hyperlink" href="#">Link</a>
-    <a class="hyperlink" href="#">Link</a>
-    <a class="hyperlink" href="#">Link</a>
-    <a class="hyperlink" href="#">Link</a>
-    <a class="hyperlink" href="#">Link</a>
+    <a class="hyperlink" href="#">Book a studio</a>
+    <a class="hyperlink" href="#">Book a session</a>
+    <a class="hyperlink" href="#">Our studios</a>
+    <a class="hyperlink" href="#">The vision</a>
+    <a class="hyperlink" href="#">Our team</a>
+    <a class="hyperlink" href="#">Contact us</a>
+    <a class="hyperlink" href="#">Faq</a>
 `;
 
 const openMenu = document.createElement('div');
