e76ac63ea226c3a6b1933ca674a9b666e91e9496
Fanny Holmström <fannyholmstrom@icloud.com>
2024-04-17 09:25:19 +0200

    :sparkles: added function to open and close menu

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import './scss/style.scss';
 
 const menuButton = document.querySelector('.menu-closed');
-const menuLinks = document.createElement('div');
 
+// Create the navigation links for the open menu
+const menuLinks = document.createElement('div');
 menuLinks.className = 'menu-links';
 menuLinks.innerHTML = `
     <a class="hyperlink" href="#">Book a studio</a>
@@ -14,13 +15,24 @@ menuLinks.innerHTML = `
     <a class="hyperlink" href="#">Faq</a>
 `;
 
+// Create the close menu icon
+const closeMenuIcon = document.createElement('img');
+closeMenuIcon.src = './assets/menu/close-menu-svg.png';
+closeMenuIcon.setAttribute('width', '35')
+closeMenuIcon.setAttribute('height', '35')
+
+// Create the open menu 
 const openMenu = document.createElement('div');
 openMenu.className = 'open-menu hidden';
 openMenu.appendChild(menuLinks);
+openMenu.appendChild(closeMenuIcon);
 document.body.appendChild(openMenu);
 
-
+// Event listeners for the open and close menu icons
 menuButton.addEventListener('click', () => {
     openMenu.classList.remove('hidden');
 });
 
+closeMenuIcon.addEventListener('click', () => {
+    openMenu.classList.add('hidden');
+});
